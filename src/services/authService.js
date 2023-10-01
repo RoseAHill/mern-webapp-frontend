f6c8f9355ac00c3377a6ab5381618485c9147b26
RosieAHill <Roseaahill@gmail.com>
2023-10-01 08:59:30 -0400

    api calls from services

---
+++ src/services/authService.js
@@ -0,0 +1,67 @@
+import * as tokenService from './tokenService'
+
+const BASE_URL = `${import.meta.env.VITE_BACKEND_SERVER_URL}/api/auth`
+
+async function signup(signupFormData) {
+  try {
+    const res = await fetch(`${BASE_URL}/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(signupFormData)
+    })
+    const json = await res.json()
+
+    if (json.err) throw new Error(json.err)
+
+    if (json.token) {
+      tokenService.setToken(json.token)
+    }
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+function getUser() {
+  return tokenService.getUserFromToken()
+}
+
+function logout() {
+  tokenService.removeToken()
+}
+
+async function login(loginFormData) {
+  try {
+    const res = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginFormData),
+    })
+    const json = await res.json()
+
+    if (json.err) throw new Error(json.err)
+
+    if (json.token) tokenService.setToken(json.token)
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+async function changePassword(changePasswordFormData) {
+  try {
+    const res = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(changePasswordFormData),
+    })
+    const json = await res.json()
+
+    if (json.err) throw new Error(json.err)
+
+    if (json.token) tokenService.setToken(json.token)
+  } catch (err) {
+    throw new Error(err)
+  }
+}
