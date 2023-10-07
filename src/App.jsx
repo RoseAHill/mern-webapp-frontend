97cefbbc074cd45c46b3b57b7a344bbdbe0f7224
RosieAHill <Roseaahill@gmail.com>
2023-10-06 20:03:24 -0400

    setup auth functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useNavigate } from 'react-router-dom'
 
 // services
 import * as authService from './services/authService'
@@ -7,17 +7,28 @@ import * as authService from './services/authService'
 // pages
 
 // components
-import NavBar from './components/Nav/NavBar'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   const [user, setUser] = useState(authService.getUser())
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    authService.logout()
+    setUser(null)
+    navigate('/')
+  }
+
+  const handleAuthEvent = () => {
+    setUser(authService.getUser())
+  }
 
   return (
       <div className="app grid grid-cols-[1fr_4fr_1fr] grid-rows-[100px_1fr_80px] h-screen bg-slate-700 text-slate-50">
-        <NavBar />
+        <NavBar user={user} handleLogout={handleLogout} />
         <main className='col-start-2 col-span-1 mt-8 pt-4 flex place-content-center'>
           <Routes>
-            <Route path='/' element={<h1>Home</h1>} />
+            <Route path='/' element={<h1>Landing</h1>} />
             <Route path='/auth/login' element={<h1>Login</h1>} />
             <Route path='/auth/signup' element={<h1>Signup</h1>} />
           </Routes>
