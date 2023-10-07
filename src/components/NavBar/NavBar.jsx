75bad8b76a133c76fb9e12f7511b64a46ee5ca89
RosieAHill <Roseaahill@gmail.com>
2023-10-06 20:06:16 -0400

    rename and add styled navlinks to navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+import NavButtonLink from "./NavButtonLink";
+import * as text from "../formatting/Text";
+import { NavLink } from "react-router-dom";
+
+const NavBar = ({ user, handleLogout }) => {
+  return (
+    <nav className="flex justify-between items-center p-4 w-screen col-span-full align-baseline">
+      <div className="nav-left w-1/3 text-left">
+          <text.Title tw={"px-1"}>
+            <NavLink className={({isActive}) => 
+          isActive ? "text-amber-200" : ""
+        } to={"/"}>T.A.</NavLink>
+          </text.Title>
+      </div>
+      <div className="nav-center w-1/3 text-center">
+        <text.Subtitle>Hello{user ? ` ${user.name}.` : "."}</text.Subtitle>
+      </div>
+      <text.Label tw="nav-right w-1/3 flex flex-row justify-end">
+        {user ? (
+          <>
+            <NavButtonLink text={"Logout"} callback={handleLogout} />
+          </>
+        ) : (
+          <>
+            <NavButtonLink text={"Login"} destination={"/auth/login"} />
+            <NavButtonLink text={"Signup"} destination={"/auth/signup"} />
+          </>
+        )}
+      </text.Label>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/components/NavBar/NavButtonLink.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const NavButtonLink = ({ text, destination = null, callback = null }) => {
+  return (
+    <div className="navButton px-1 mx-3 underline underline-offset-1 font-bold hover:no-underline hover:cursor-pointer">
+      {destination ? (
+        <NavLink className={({isActive}) => 
+          isActive ? "text-amber-200" : ""
+        } to={destination}>{text}</NavLink>
+      ) : (
+        <Link to="" onClick={callback}></Link>
+      )}
+    </div>
+  );
+};
+
+export default NavButtonLink;
