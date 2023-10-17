4f55e80772e601b9b57d86149eaeaa6d681310eb
RosieAHill <Roseaahill@gmail.com>
2023-10-17 12:26:55 -0400

    refactored text styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,20 @@
 import NavButtonLink from "./NavButtonLink";
-import * as text from "../formatting/Text";
 import { NavLink } from "react-router-dom";
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <nav className="flex justify-between items-center p-4 w-screen col-span-full align-baseline">
       <div className="nav-left w-1/3 text-left">
-          <text.Title tw={"px-1"}>
+          <div className="text-3xl font-bold px-1">
             <NavLink className={({isActive}) => 
           isActive ? "text-amber-200" : ""
         } to={"/"}>T.A.</NavLink>
-          </text.Title>
+          </div>
       </div>
       <div className="nav-center w-1/3 text-center">
-        <text.Subtitle>Hello{user ? ` ${user.username}.` : "."}</text.Subtitle>
+        <div className="text-lg font-medium italic">Hello{user ? ` ${user.username}.` : "."}</div>
       </div>
-      <text.Label tw="nav-right w-1/3 flex flex-row justify-end">
+      <div className="font-mono nav-right w-1/3 flex flex-row justify-end">
         {user ? (
           <>
             <NavButtonLink text={"Logout"} callback={handleLogout} />
@@ -26,7 +25,7 @@ const NavBar = ({ user, handleLogout }) => {
             <NavButtonLink text={"Signup"} destination={"/auth/signup"} />
           </>
         )}
-      </text.Label>
+      </div>
     </nav>
   );
 };
